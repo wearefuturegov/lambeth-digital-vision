9660f6ec6aadd171f1ad7e04053a14e5df08f929
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-05-31 13:41:25 +0100

    start using scrollreveal

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,11 @@
 import $ from "jquery"
 import "slick-carousel"
 
+import ScrollReveal from "scrollreveal"
+
 $(document).ready(function(){
     $(".slider__carousel")
         .slick({dots:true})
 })
+
+ScrollReveal().reveal('.list__item')
